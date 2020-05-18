329759bc2a89e76dc487e652759aa5fc3355085c
Jackson Smith <jacksonsmith7343@gmail.com>
2020-05-18 14:59:49 -0500

    Initial commit.

---
+++ MyFavoriteSuperHero_Project/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyFavoriteSuperHero_Project.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            DbSet<Hero> Heroes;
+
+        }
+    }
+}
---
+++ MyFavoriteSuperHero_Project/Models/Hero.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFavoriteSuperHero_Project.Models
+{
+    public class Hero
+    {
+        public string Name { get; set; }
+
+        public string AlterEgo { get; set; }
+
+        public string PrimarySuperheroAbility { get; set; }
+
+        public string SecondarySuperheroAbility { get; set; }
+
+        public string CatchPhrase { get; set; }
+    }
+}
