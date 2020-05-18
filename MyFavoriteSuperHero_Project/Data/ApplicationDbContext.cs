9f2b27638025b524e7122ce804062b72607d0d3a
Jackson Smith <jacksonsmith7343@gmail.com>
2020-05-18 15:36:36 -0500

    Added a controller in the controllers folder.

--- MyFavoriteSuperHero_Project/Data/ApplicationDbContext.cs
+++ MyFavoriteSuperHero_Project/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using MyFavoriteSuperHero_Project.Models;
 
 namespace MyFavoriteSuperHero_Project.Data
 {
